f6ec928a7839240ff80fdc41e83af16fd546b164
Wellington <wtpereira@gmail.com>
2025-01-23 19:32:49 -0300

    Excluir a pasta 'aulas' da branch 'main'.

--- aulas/aula02_05.py
+++
@@ -1,5 +0,0 @@
-base = float(input('Digite o valor da base do retângulo em cm: '))
-
-altura = float(input('Digite o valor da altura do retângulo em cm: '))
-
-print(f'O valor da área é {base * altura}')
