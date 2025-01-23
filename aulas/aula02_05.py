22fea9931e238c1f71842d3785c9540dfc4b8577
Wellington <wtpereira@gmail.com>
2025-01-22 21:48:40 -0300

    Criar o arquivo 'aula02_05.py'.

---
+++ aulas/aula02_05.py
@@ -0,0 +1,5 @@
+base = float(input('Digite o valor da base do retângulo em cm: '))
+
+altura = float(input('Digite o valor da altura do retângulo em cm: '))
+
+print(f'O valor da área é {base * altura}')
