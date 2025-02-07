7b1a7cd08270f1b42570dc9890baa5f77531bf07
Wellington <wtpereira@gmail.com>
2025-02-06 21:56:47 -0300

    Criar o módulo 'model'.

---
+++ model/autor.py
@@ -0,0 +1,64 @@
+from model.utils import valida_email
+
+
+class Autor:
+    id_autor = 1  # atributo de classe
+
+    __slots__ = ['__id', '__nome', '__email', '__fone', '__biografia']
+    def __init__(self, nome: str, fone: str=None, biografia: str=None):  # construtor
+        self.id = Autor.id_autor
+        self.nome = nome
+        self.__email = None
+        self.fone = fone
+        self.biografia = biografia
+
+    def __str__(self):
+        return f'{self.id} | {self.nome} | {self.email} | {self.fone} | {self.biografia}'
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.nome}", "{self.fone}", "{self.biografia}")'
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
+        Autor.id_autor += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome: str):
+        self.__nome = nome.title()
+
+    @property
+    def email(self):  # getter
+        return self.__email
+
+    @email.setter
+    def email(self, em: str):  # setter
+        if valida_email(em):
+            self.__email = em.lower()
+            return
+        ex = Exception('E-mail inválido!')
+        raise ex
+
+    @property
+    def fone(self):
+        return self.__fone
+
+    @fone.setter
+    def fone(self, fone):
+        self.__fone = fone
+
+    @property
+    def biografia(self):
+        return self.__biografia
+
+    @biografia.setter
+    def biografia(self, biografia):
+        self.__biografia = biografia
---
+++ model/utils.py
@@ -0,0 +1,6 @@
+def valida_email(email: str) -> bool:
+    email = email.lower()
+    if email.find('@') >=0 and email.endswith('.com'):
+        return True
+
+    return False
